4321aeb7bd27eba09ba28fc3295f8ed27afcca3f
Thomas Gorny <thomas.gorny@pm.me>
2018-11-06 17:10:07 +0100

    inprove existing investment test, add a valid value so that investment was possible with enough balance otherwise

--- app/test-e2e/x-eto/InvestWithNoMoney.spec.e2e.ts
+++ app/test-e2e/x-eto/InvestWithNoMoney.spec.e2e.ts
@@ -1,6 +1,6 @@
-import { createAndLoginNewUser } from "../utils/userHelpers";
-import { tid } from "../utils";
 import { INV_EMPTY_HAS_KYC } from "../constants";
+import { tid } from "../utils";
+import { createAndLoginNewUser } from "../utils/userHelpers";
 import { waitUntilEtoIsInState } from "./utils";
 
 const PUBLIC_ETO_ID = "0x560687Db44b19Ce8347A2D35873Dd95269dDF6bC";
@@ -18,7 +18,7 @@ describe("Try and invest without money", () => {
       cy.get(tid("eto-invest-now-button-" + PUBLIC_ETO_ID)).click();
       cy.get(tid("invest-modal-eth-field"))
         .clear()
-        .type("10");
+        .type("200");
       cy.wait(1000);
       cy.get(tid("invest-modal-invest-now-button")).should("be.disabled");
     });
