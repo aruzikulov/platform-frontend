0015f8a6f30c852cb6822ddc7e8aa372213a09f8
Thomas Gorny <thomas.gorny@pm.me>
2018-11-07 17:59:50 +0100

    improve bank transfer test

--- app/test-e2e/x-eto/InvestWithBankTransfer.spec.e2e.ts
+++ app/test-e2e/x-eto/InvestWithBankTransfer.spec.e2e.ts
@@ -1,5 +1,4 @@
 import { INV_EUR_ICBM_HAS_KYC_SEED } from "../constants";
-import { confirmAccessModal } from "../utils/index";
 import { tid } from "../utils/selectors";
 import { createAndLoginNewUser } from "../utils/userHelpers";
 
@@ -21,13 +20,27 @@ describe("Invest with full icbm wallet", () => {
 
       cy.get(tid("invest-modal-eur-field"))
         .clear()
-        .type("1000");
+        .type("123");
       cy.wait(1000);
 
+      cy.get(tid("invest-modal-gas-cost")).should($e => {
+        expect($e.text()).to.contain("ETH 0.0000"); // no gas cost on bank transfer
+      });
       cy.get(tid("invest-modal-invest-now-button")).click();
+
+      cy.get(tid("invest-modal-bank-transfer-summary-amount")).should($m => {
+        expect($m.text())
+          .to.contain("123")
+          .and.to.contain("€");
+      });
+
       cy.get(tid("invest-modal-summary-confirm-button")).click();
-      cy.get(tid("investment-flow.success.title"));
-      // TODO check smart contracts balances
+      cy.get(tid("invest-modal-bank-transfer-details-title"));
+      cy.get(tid("invest-modal-bank-transfer-details-amount")).should($m => {
+        expect($m.text().trim())
+          .to.contain("123")
+          .and.length(6); // 123.00 or 123,00
+      });
     });
   });
 });
